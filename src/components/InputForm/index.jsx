e77e26b023e3ac1684e09135166e770d19212181
Luiza <luizadirias@gmail.com>
2025-03-21 16:08:47 -0300

    Adiciona tela de cadastro de despesas

--- src/components/InputForm/index.jsx
+++ src/components/InputForm/index.jsx
@@ -3,9 +3,9 @@ import styles from './index.module.scss'
 
 const InputForm = ({tipo, placeholder, label}) => {
   return (
-    <div>
+    <div className={styles.fundo}>
         <label className= {styles.label}>{label}</label>
-        <input className={styles.input} type= {tipo} placeholder={placeholder}/>
+        <input className={styles.input} step="0.01" type= {tipo} pattern="^\$\d{1,3}(,\d{3})*(\.\d+)?$" placeholder={placeholder}/>
     </div>
   )
 }
